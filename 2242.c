b578bb92d98a2fc572113abe754c1ce94e53e386
llangeani <69474888+llangeani@users.noreply.github.com>
2020-08-21 20:30:52 -0300

    Update 2242.c

--- 2242.c
+++ 2242.c
@@ -11,12 +11,12 @@ Função: extrair as vogais de uma
  
 int main() {
  
-	// declaração de variáveis
+    // declaração de variáveis
     char rs[51], t[51];
     int i, j;		
     
-	//	lendo o valor de uma string com scanf e um vetor 
-	//	pesquisa de vogais utilizando contadores
+    //	lendo o valor de uma string com scanf e um vetor 
+    //	pesquisa de vogais utilizando contadores
     scanf("%s", rs);
     for(i = 0, j = 0; rs[i] != '\0'; i++)	
 		if(rs[i] == 'a' || rs[i] == 'e' || rs[i] == 'i' || rs[i] == 'o' || rs[i] == 'u')
@@ -28,10 +28,10 @@ int main() {
 	//	preparando contadores para varrer os elementos do vetor vogais
 	i = 0;
 		
-	//	j é o número de vogais, então o índice dos elementos é j-1
-	//	teste para igualdade dos elementos equidistante das pontas
-	//	para um número par de vogais não há elemento central
-	//	para um número ímpar de vogais, o elemento central tem índice j/2
+    //	j é o número de vogais, então o índice dos elementos é j-1
+    //	teste para igualdade dos elementos equidistante das pontas
+    //	para um número par de vogais não há elemento central
+    //	para um número ímpar de vogais, o elemento central tem índice j/2
     if(j % 2 == 0)
 		while (i < j-1 && t[i] == t[j-1])
 		{
@@ -45,8 +45,8 @@ int main() {
 			j--;
 		}
 	
-	// vogais equidistantes, imprime S
-	// vogais não-equidistantes, imprime N
+	// vogais equidistantes iguais, imprime S
+	// vogais equidistantes diferentes, imprime N
 	if(t[i] == t[j-1])	
 		printf("S\n");
 	else

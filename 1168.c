3f0b32decd2de6d7aee755258b0793ca41933d65
llangeani <69474888+llangeani@users.noreply.github.com>
2020-08-25 23:46:08 -0300

    formatação

--- 1168.c
+++ 1168.c
@@ -11,23 +11,23 @@ representar números de 1 a 10^100
 
 int main()
 {
-    int  N,       //número N de casos
-	     qtdLED,  //quantidade total de LEDS necessários
-		 i,       //contador de casos
-		 j,       //contador para varrer matriz valor
-	     LED[10]  //matriz com quantidade de LEDS para cada número
-		 = {6,2,5,5,4,5,6,3,7,6}; //LED[0] = qtd de LEDS para 0
+    int  N,	//número N de casos
+    qtdLED,     //quantidade total de LEDS necessários
+         i,     //contador de casos
+	 j,	//contador para varrer matriz valor
+   LED[10]	//matriz com quantidade de LEDS para cada número
+   = {6,2,5,5,4,5,6,3,7,6}; //LED[0] = qtd de LEDS para 0
 		 
-    char valor[101];  //matriz para capturar valor de até 10^100 e \0
+    char valor[101];	//matriz para capturar valor de até 10^100 e elemento nulo '\0'
     
-	scanf("%d", &N);
-	for (i = 0; i < N; i++)
-	{
-		scanf("%s", valor);
-		for(j = 0, qtdLED = 0; valor[j] != '\0'; j++)  //condição de parada é final da string ('\0')
-			qtdLED += LED[(int)valor[j] - 48];         //subtrair 48 - transformação de valor dos
-		printf("%d leds\n", qtdLED);                   //números unitários em ASCII para inteiro
-	}
+    scanf("%d", &N);
+    for (i = 0; i < N; i++)
+    {
+	scanf("%s", valor);
+	for(j = 0, qtdLED = 0; valor[j] != '\0'; j++)  //condição de parada é final da string ('\0')
+            qtdLED += LED[(int)valor[j] - 48];         //subtrair 48 - transformação de valor dos
+	printf("%d leds\n", qtdLED);                   //números unitários em ASCII para inteiro
+    }
 	
     return 0;
 }

625b2ba10aea09985e519d6e4b0be5f530ad6c7f
llangeani <69474888+llangeani@users.noreply.github.com>
2020-08-25 23:42:13 -0300

    formatação

--- 1871.c
+++ 1871.c
@@ -20,18 +20,18 @@ int main()
 		i;	//contador
 	
 	while (scanf("%d %d", &M, &N)	//entrada de dois números
-				   , M && N != 0)	//condição de parada - números são nulos
+			, M && N != 0)	//condição de parada - números são nulos
 	{
 		S = M + N;
 			
-		for (i = 0, S0 = 0; S != 0 ; )		//algoritmo para testar o número
-		{									//da casa dos decimais, um a um
-			if (S % 10 != 0)				//há incremento do valor S se a
-			{								//casa decimal de S é != de 0
+		for (i = 0, S0 = 0; S != 0 ; )			//algoritmo para testar o número
+		{						//da casa dos decimais, um a um
+			if (S % 10 != 0)			//há incremento do valor S se a
+			{					//casa decimal de S é != de 0
 				S0 += S % 10 * pow(10, i);  //o contador i é a ordem 10^i
-				i++;						//do valor incrementado
+				i++;			    //do valor incrementado
 			}
-			S = S / 10;						//diminui a ordem da soma S
+			S = S / 10;   //diminui a ordem da soma S
 		}
 		printf("%d\n", S0);
 	}
